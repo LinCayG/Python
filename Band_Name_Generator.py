640d6d02fe54cc2aea8763d8f0c102bd1dda6d01
Lindsay <Lindsay.Gilley@gmail.com>
2022-08-02 20:38:56 -0500

    Add Band_Name_Generator.py

---
+++ Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print('Hello, we are going to generate a band name for you')
+b=input('What city did you grow up in? ')
+p=input('What is a name of one of your pets? ')
+print('Your band name recommendation is The ' + b + ' ' + p + 's')
