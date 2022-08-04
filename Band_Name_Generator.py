9377b6c6b469f6d8efc928e213e343aa833f0a67
Lindsay <Lindsay.Gilley@gmail.com>
2022-08-03 21:50:28 -0500

    Added new lines and changed variable names to be easier to read

--- Band_Name_Generator.py
+++ Band_Name_Generator.py
@@ -1,4 +1,4 @@
 print('Hello, we are going to generate a band name for you')
-b=input('What city did you grow up in? ')
-p=input('What is a name of one of your pets? ')
-print('Your band name recommendation is The ' + b + ' ' + p + 's')
+City_Name=input('What city did you grow up in?\n')
+Pet_Name=input('What is a name of one of your pets?\n ')
+print('Your band name recommendation is The ' + City_Name + ' ' + Pet_Name + 's')
