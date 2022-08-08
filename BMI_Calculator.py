394e8202dde3ae4a9ec9e0107ef741dae21f9f76
Lindsay <Lindsay.Gilley@gmail.com>
2022-08-07 21:01:20 -0500

    Updated BMI Calcuator to add a message about the user's BMI number.

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -3,3 +3,13 @@ Height = input('Please enter your height in inches: \n')
 Weight = input('Please enter your weight in pounds: \n')
 BMI = round((int(Weight)*703/(int(Height)*int(Height))),1)
 print(f'Your BMI is calculated as: {BMI}')
+if BMI < 18.5:
+    print('You are considered underweight.')
+elif BMI <25:
+    print('You are considered to be in a normal weight range.')
+elif BMI <30:
+    print('You are considered overweight.')
+elif BMI < 35:
+    print('You are considered obese.')
+else:
+    print('You are considered clinically obese.')
