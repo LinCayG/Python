b007e5be816465dde7ed6b60d7f0bba020d60b2f
Lindsay <Lindsay.Gilley@gmail.com>
2022-11-25 10:45:16 -0600

    Updated Caesar_Cipher to optimize functions

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,31 +1,27 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt(orig_text, shift_num):
+def caesar(ci_text, shift_num, ci_direction):
     cipher_text = ""
-    for i in orig_text:
-        pos = alphabet.index(i)
-        new_pos = pos+shift_num
-        new_letter = alphabet[new_pos]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+    if ci_direction == "decode":
+        shift_num *=-1
+    for char in ci_text:
+        if char in alphabet:
+            pos = alphabet.index(char)
+            new_pos = pos+shift_num
+            cipher_text += alphabet[new_pos]
+        else:
+            cipher_text +=char
 
-def decrypt(de_text, de_shift):
-    cipher_text = ""
-    for i in de_text:
-        pos = alphabet.index(i)
-        new_pos = pos-de_shift
-        new_letter = alphabet[new_pos]
-        cipher_text += new_letter
-    print(f"The decoded text is {cipher_text}")
+    print(f"The {ci_direction}d text is {cipher_text}")
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-if direction == "encode":
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number(between 0 and 26):\n"))
-    encrypt(orig_text=text,shift_num=shift)
-elif direction == "decode":
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number(between 0 and 26):\n"))
-    decrypt(de_text=text,de_shift=shift)
-else:
-    print("Invalid input.")
+    shift = int(input("Type the shift number:\n")) % 26
+    caesar(ci_text=text,shift_num=shift, ci_direction=direction)
+    result = input("Type 'yes' if you want to go again.  Otherwise type 'no'.\n")
+    if result =="no":
+        should_continue = False
+        print("Thanks and have a great day!")
+
