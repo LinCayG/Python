c8981acf209c81aa9d545c12368c64c43766ee3a
Lindsay <Lindsay.Gilley@gmail.com>
2023-01-17 18:30:23 -0600

    Added a recursive function

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -21,17 +21,22 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What is the first number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What is the second number?: "))
-calculation_function = operations[operation_symbol]
-first_answer=calculation_function(num1,num2)
-print(f"{num1}{operation_symbol}{num2}={first_answer}")
+def calculator():
+    num1 = int(input("What is the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-operation_symbol = input("Pick another operation: ")
-num3=int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-second_answer=calculation_function(first_answer,num3)
-print(f"{first_answer}{operation_symbol}{num3}={second_answer}")
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = int(input("What is the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer=calculation_function(num1,num2)
+        print(f"{num1}{operation_symbol}{num2}={answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+calculator()
