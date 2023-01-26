b0d223247f5908b063f6757512f590f5351337bb
Lindsay <Lindsay.Gilley@gmail.com>
2023-01-25 19:41:36 -0600

    Finished Higher_Lower_Game functionality for a complete game

--- Higher_Lower_Game.py
+++ Higher_Lower_Game.py
@@ -3,14 +3,10 @@
 import random
 from HL_Logo import logo, vs
 from HL_game_data import data
+import os
 
+score = 0
 print(logo)
-PersonA = random.choice(data)
-PersonB = random.choice(data)
-if PersonA == PersonB: #ensures there will be 2 different entities to compare
-    while PersonA==PersonB: #continues to loop until there are 2 different entities
-        PersonB = random.choice(data)
-
 #Bring up information about the celebrities from the dictionary
 def format_person(Person):
     Person_name = Person["name"]
@@ -18,31 +14,50 @@ def format_person(Person):
     Person_country = Person["country"]
     return f"{Person_name}, a(n) {Person_descr}, from {Person_country}"
 
-#take input on who is higher
-print(f"Compare A: {format_person(PersonA)}.")
-print(vs)
-print(f"Against B: {format_person(PersonB)}.")
-
-guess = input("Who has more followers? Type 'A' or 'B': ").upper()
 #if statement to determine who has more followers and continue if user guessed correctly using the dictionary keys:value
 def follower_count(Count):
     Person_count = Count["follower_count"]
     return Person_count
 
-FollowerA = follower_count(PersonA)
-FollowerB = follower_count(PersonB)
-winner = max(FollowerA, FollowerB)
+game_should_continue = True
+PersonB = random.choice(data)
 
-if guess == 'A':
-    choice = FollowerA
-else:
-    choice = FollowerB
+while game_should_continue:
 
-if winner == choice:
-    print('you won')
-else:
-    print('you lose')
+    PersonA = PersonB
+    PersonB = random.choice(data)
+    if PersonA == PersonB: #ensures there will be 2 different entities to compare
+        while PersonA==PersonB: #continues to loop until there are 2 different entities
+            PersonB = random.choice(data)
 
-#if user guessed correctly, have an ongoing count, with count = 0 to begin and assign the correct guess to PersonA
-#when user guesses wrong, print the number of correct guesses
-#ask the user if they want to play again - use a while loop with a boolean
+    #take input on who is higher
+    print(f"Compare A: {format_person(PersonA)}.")
+    print(vs)
+    print(f"Against B: {format_person(PersonB)}.")
+
+    guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+    os.system('cls')
+    print(logo)
+
+    FollowerA = follower_count(PersonA)
+    FollowerB = follower_count(PersonB)
+    winner = max(FollowerA, FollowerB)
+
+    if guess == 'A':
+        choice = FollowerA
+    else:
+        choice = FollowerB
+
+    if winner == choice:
+        is_correct = True
+    else:
+        is_correct = False
+
+    if is_correct:
+        score +=1
+        print(f"You're right!  Current score: {score}.")
+    else:
+        print(f"Sorry, that's wrong.  Final score: {score}.")
+        game_should_continue = False
+
