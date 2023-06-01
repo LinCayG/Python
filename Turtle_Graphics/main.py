c457d0d671a5b791846a11f7a03f259b5d924c48
Lindsay <Lindsay.Gilley@gmail.com>
2023-05-31 22:08:33 -0500

    Added drawing shapes and randomizing line colors to shape drawings.

--- Turtle_Graphics/main.py
+++ Turtle_Graphics/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle() #creates a turtle object to use
 tim.shape("turtle")
@@ -14,7 +15,18 @@ for length in range(15): #draws a dashed line
     tim.forward(10)
     tim.pd()
 
+#Drawing various shapes
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
 
+colors = ["light steel blue","sienna", "spring green", "red", "indigo", "dark green", "gold", "aquamarine", "black"]
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
 
 
 
