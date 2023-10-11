d98a369491691eda3efce5ef7000371e8bcc5ff6
Lindsay <Lindsay.Gilley@gmail.com>
2023-10-11 18:51:14 -0500

    Added the Pong ball and detects collision with the top/bottom as well as with both ppaddles

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 rtpaddle = Paddle((350, 0))
 ltpaddle = Paddle((-350,0))
+ball = Ball()
 
 
 
@@ -22,6 +25,15 @@ screen.onkey(ltpaddle.go_down,"s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280: #detecting collision with wall
+        ball.bounce_y()
+
+    # detecting collision with right and left paddle
+    if ball.distance(rtpaddle) < 50 and ball.xcor() > 320 or ball.distance(ltpaddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
