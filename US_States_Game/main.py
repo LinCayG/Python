18088d3974b4b8e4d5c71683c13bb6d42c1b3472
Lindsay <Lindsay.Gilley@gmail.com>
2024-01-09 13:48:56 -0600

    Finished US States Game and it creates a csv file of missing states

--- US_States_Game/main.py
+++ US_States_Game/main.py
@@ -1,8 +1,44 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
+#can use this to get coordinates on the image file, which is currently in the csv file
+#def get_mouse_click_coor(x, y):
+#    print(x, y)
+#turtle.onscreenclick(get_mouse_click_coor)
+#turtle.mainloop() #keeps screen open even though we are clicking
+#end of coordinates for the image file
 
-screen.exitonclick()
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)} out of 50 States Correct", prompt="What's another state's name? ").title()
+    print(answer_state)
+
+#Is the answer_state one of the states in the csv?
+#If correct, need to create a turtle with the name at the x,y state coordinate
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("States_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
