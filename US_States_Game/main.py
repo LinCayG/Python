730a59adaacb9e8eb7ece30ce86a9f2143e06d2c
Lindsay <Lindsay.Gilley@gmail.com>
2024-01-09 14:42:10 -0600

    Condensed code by using conditional list comprehension

--- US_States_Game/main.py
+++ US_States_Game/main.py
@@ -26,10 +26,7 @@ while len(guessed_states) < 50:
 #Is the answer_state one of the states in the csv?
 #If correct, need to create a turtle with the name at the x,y state coordinate
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("States_to_learn.csv")
         break
